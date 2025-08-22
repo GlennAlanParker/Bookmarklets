74ed802036872f8e946498e341e2f8984f189999
GlennAlanParker <127654597+GlennAlanParker@users.noreply.github.com>
2025-08-22 15:31:36 -0500

    Update visuallyHidden.js

--- visuallyHidden.js
+++ visuallyHidden.js
@@ -1,13 +1,15 @@
-document.querySelectorAll('.vh-content, .vh-content-off').forEach(el => {
-    if (el.classList.contains('vh-content')) {
-        el.classList.remove('vh-content');
-        el.classList.add('vh-content-off');
-        el.style.setProperty('background-color', 'yellow', 'important'); // Yellow highlight
-        el.style.setProperty('color', 'black', 'important');             // Black text
-    } else if (el.classList.contains('vh-content-off')) {
-        el.classList.remove('vh-content-off');
-        el.classList.add('vh-content');
-        el.style.removeProperty('background-color'); // Reset
-        el.style.removeProperty('color');            // Reset
-    }
-});
+javascript:(() => {
+    document.querySelectorAll('.vh-content, .vh-content-off').forEach(el => {
+        if (el.classList.contains('vh-content')) {
+            el.classList.remove('vh-content');
+            el.classList.add('vh-content-off');
+            el.style.setProperty('background-color', 'yellow', 'important'); // Yellow highlight
+            el.style.setProperty('color', 'black', 'important');             // Black text
+        } else if (el.classList.contains('vh-content-off')) {
+            el.classList.remove('vh-content-off');
+            el.classList.add('vh-content');
+            el.style.removeProperty('background-color'); // Reset
+            el.style.removeProperty('color');            // Reset
+        }
+    });
+})();
