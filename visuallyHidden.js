3a72a98000628f415397304ea5fd97aaeba822e5
GlennAlanParker <127654597+GlennAlanParker@users.noreply.github.com>
2025-09-12 07:43:32 -0500

    Update visuallyHidden.js

--- visuallyHidden.js
+++ visuallyHidden.js
@@ -1,15 +1,26 @@
 javascript:(() => {
-    document.querySelectorAll('.vh-content, .vh-content-off').forEach(el => {
-        if (el.classList.contains('vh-content')) {
-            el.classList.remove('vh-content');
-            el.classList.add('vh-content-off');
-            el.style.setProperty('background-color', 'yellow', 'important'); // Yellow highlight
-            el.style.setProperty('color', 'black', 'important');             // Black text
-        } else if (el.classList.contains('vh-content-off')) {
-            el.classList.remove('vh-content-off');
-            el.classList.add('vh-content');
-            el.style.removeProperty('background-color'); // Reset
-            el.style.removeProperty('color');            // Reset
-        }
-    });
+    function toggleVHContent(doc) {
+        doc.querySelectorAll('.vh-content, .vh-content-off').forEach(el => {
+            if (el.classList.contains('vh-content')) {
+                el.classList.remove('vh-content');
+                el.classList.add('vh-content-off');
+                el.style.setProperty('background-color', 'yellow', 'important'); // Yellow highlight
+                el.style.setProperty('color', 'black', 'important');             // Black text
+            } else if (el.classList.contains('vh-content-off')) {
+                el.classList.remove('vh-content-off');
+                el.classList.add('vh-content');
+                el.style.removeProperty('background-color'); // Reset
+                el.style.removeProperty('color');            // Reset
+            }
+        });
+    }
+
+    // Run on main document
+    toggleVHContent(document);
+
+    // Also run inside iframe (if present)
+    const iframe = document.getElementById('ifPreview');
+    if (iframe && iframe.contentDocument) {
+        toggleVHContent(iframe.contentDocument);
+    }
 })();
