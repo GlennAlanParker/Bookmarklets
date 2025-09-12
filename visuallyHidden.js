72480be9d0a67d050e1ceb75a0312fdf65ce5efc
GlennAlanParker <127654597+GlennAlanParker@users.noreply.github.com>
2025-09-12 07:48:48 -0500

    Update visuallyHidden.js

--- visuallyHidden.js
+++ visuallyHidden.js
@@ -1,32 +1,15 @@
 javascript:(() => {
-    function toggleVHContent(doc) {
-        doc.querySelectorAll('.vh-content, .vh-content-off').forEach(el => {
-            if (el.classList.contains('vh-content')) {
-                el.classList.remove('vh-content');
-                el.classList.add('vh-content-off');
-                el.style.setProperty('background-color', 'yellow', 'important');
-                el.style.setProperty('color', 'black', 'important');
-            } else if (el.classList.contains('vh-content-off')) {
-                el.classList.remove('vh-content-off');
-                el.classList.add('vh-content');
-                el.style.removeProperty('background-color');
-                el.style.removeProperty('color');
-            }
-        });
-    }
-
-    // Main page
-    toggleVHContent(document);
-
-    // Wait for iframe load
-    const iframe = document.getElementById('ifPreview');
-    if (iframe) {
-        iframe.addEventListener('load', () => {
-            try {
-                toggleVHContent(iframe.contentDocument);
-            } catch(e) {
-                alert("Can't access iframe (cross-origin).");
-            }
-        });
-    }
+    document.querySelectorAll('.vh-content, .vh-content-off').forEach(el => {
+        if (el.classList.contains('vh-content')) {
+            el.classList.remove('vh-content');
+            el.classList.add('vh-content-off');
+            el.style.setProperty('background-color', 'yellow', 'important'); // Yellow highlight
+            el.style.setProperty('color', 'black', 'important');             // Black text
+        } else if (el.classList.contains('vh-content-off')) {
+            el.classList.remove('vh-content-off');
+            el.classList.add('vh-content');
+            el.style.removeProperty('background-color'); // Reset
+            el.style.removeProperty('color');            // Reset
+        }
+    });
 })();
